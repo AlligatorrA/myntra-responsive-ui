fda571127591a82ac97cc92423539cf243034a7d
AlligatorA <anandraj143kick@gmail.com>
2022-07-01 15:06:38 +0530

    style : footer CSS

--- components/navbar/Footer.js
+++ components/navbar/Footer.js
@@ -2,89 +2,92 @@ import Link from 'next/link'
 const Footer = () => {
     return (
         <div>
-            <div className="topRow">
-                <h5>ONLINE SHOPPING</h5>
-                <div className="flex items-center">
-                    <ul>
-                        <li> <Link href='/' >
-                            <a>Men</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Women</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Kids</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Home & Living</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Beauty</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Gift Card</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Myntra Insider</a>
-                        </Link></li>
-                    </ul>
-                </div>
-                <div className="">
-                    <h5>USEFUL LINKS</h5>
-                    <ul>
-                        <li><Link href='/' >
-                            <a>Contact Us</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>FAQ</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>T&C</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Terms Of Use</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Track Order</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Shipping</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Cancellation</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Return</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Whitehat</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>BLog</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Carrers</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Privacy Policy</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Site Map</a>
-                        </Link></li>
-                        <li><Link href='/' >
-                            <a>Corporate Information</a>
-                        </Link></li>
-                    </ul>
-                </div>
-                <div className="">
-                    <h5>EXPERIENCE MYNTRA APP ON MOBILE</h5>
-                </div>
-                <div className="">
-                    <h5>100% ORIGINAL</h5>
-                    <h5>100% ORIGINAL</h5>
+            <div className="flex flex-wrap gap-5  flex-col">
+                <h5 className='font-bold m-7'>ONLINE SHOPPING</h5>
+                <div className='flex  justify-around'>
+                    <div className="flex  justify-start">
+                        <ul className="flex  justify-start flex-col items-start">
+                            <li> <Link href='/' >
+                                <a>Men</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Women</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Kids</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Home & Living</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Beauty</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Gift Card</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Myntra Insider</a>
+                            </Link></li>
+                        </ul>
+                    </div>
+                    <div className="">
+                        <h5>USEFUL LINKS</h5>
+                        <ul className="flex  justify-start flex-col items-start">
+                            <li><Link href='/' >
+                                <a>Contact Us</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>FAQ</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>T&C</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Terms Of Use</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Track Order</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Shipping</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Cancellation</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Return</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Whitehat</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>BLog</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Carrers</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Privacy Policy</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Site Map</a>
+                            </Link></li>
+                            <li><Link href='/' >
+                                <a>Corporate Information</a>
+                            </Link></li>
+                        </ul>
+                    </div>
+                    <div className="">
+                        <h5>EXPERIENCE MYNTRA APP ON MOBILE</h5>
+                    </div>
+                    <div className="flex  justify-start flex-col items-start">
+                        <h5>100% ORIGINAL</h5>
+                        <h5>100% ORIGINAL</h5>
+                    </div>
                 </div>
             </div>
             <div className="bottomRow">
+                <h3 className='font-bold m-7'>PRODUCT SEARCHES</h3>
                 <Link href='/' >
                     <a>Kurta pajama</a>
                 </Link>|
@@ -110,10 +113,10 @@ const Footer = () => {
                     <a>Denim Shirt</a>
                 </Link>|
             </div>
-            <div className="">
+            <div className=" flex justify-around m-4">
                 <p>In case of any concern, <Link href='/' >
                     <a>Contact Us</a>
-                </Link>|</p>
+                </Link></p>
                 <p>© 2022 www.myntra.com. All rights reserved.</p>
             </div>
         </div>
